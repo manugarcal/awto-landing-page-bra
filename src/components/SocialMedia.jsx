c7220cdc98c8b1270915aea13641f2607999542d
malegarcal <mgarciac@outlook.it>
2021-09-26 00:59:04 -0300

    [add/update] added html and styles to social media component

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
+import iconFb from "../assets/rrss-facebook.svg";
+import iconTt from "../assets/rrss-twitter.svg";
+import iconIg from "../assets/rrss-instagram.svg";
 
 const SocialMedia = () => {
-    return (
-        <div>
-            social media
+  return (
+    <section className="socialMediaContainer">
+      <div className="socialMediaWrapper">
+        <h3 className="primaryTitle">Siga-nos em nossas redes sociais</h3>
+        <div className="socialMediaIconsContainer">
+          <div className="socialMediaIcon">
+            <img src={iconFb} alt="FaceBook" />
+          </div>
+          <div className="socialMediaIcon">
+            <img src={iconTt} alt="Twitter" />
+          </div>
+          <div className="socialMediaIcon">
+            <img src={iconIg} alt="Instagram" />
+          </div>
         </div>
-    )
-}
+      </div>
+    </section>
+  );
+};
 export default SocialMedia;

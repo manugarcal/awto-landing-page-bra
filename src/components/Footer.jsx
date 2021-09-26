9256d549cd8905b9d58971f729c43a386042f3b5
malegarcal <mgarciac@outlook.it>
2021-09-26 12:45:24 -0300

    [add/update] footer component created and styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            footer
-        </div>
-    )
-}
+  return (
+    <div className="footerContainer">
+      <div className="footerText">
+        <p className="secondaryText">
+          Awto &copy; Copyright 2021. todos os dereitos reservados
+        </p>
+      </div>
+      <div className="footerBtns">
+        <a className="footerLink" href="/#">
+          Nossos Pilares
+        </a>
+        <a className="footerLink" href="/#">
+          Contato
+        </a>
+      </div>
+    </div>
+  );
+};
 export default Footer;

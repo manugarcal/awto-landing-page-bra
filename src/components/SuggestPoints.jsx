4d49ba293d0986b8c8925f2f0f60d5efd5e98ed0
Onas Lepage <olepage@ecomsur.com>
2021-10-13 23:48:11 -0300

    css iframe

--- src/components/SuggestPoints.jsx
+++ src/components/SuggestPoints.jsx
@@ -13,47 +13,8 @@ const SuggestPoints = () => {
         <a href="" className="text-color-primary" onClick={actions.openModal()} data-modal="modal-one">instruções</a>
       </div>
       <div className="flex flex-wrap w-100 mv5">
-        <div className="w-100 w-50-l">map</div>
-        <div className="w-100 w-50-l">
-          <form>
-            <div className="w-100 w-80-l flex justify-center flex-column">
-              <label className="center text-color-primary fw7 mv2 w-100 w-70-m db">Ponto Awto 1</label>
-              <input
-                className="formControl center db w-100 w-70-m"
-                type="text"
-                placeholder="Encontrar-me na barra de endereço"
-              />
-              <label className="center text-color-primary fw7 mv2 w-100 w-70-m db">Ponto Awto 2</label>
-              <input
-                className="formControl center db w-100 w-70-m"
-                type="text"
-                placeholder="Encontrar-me na barra de endereço"
-              />
-              <label className="center text-color-primary fw7 mv2 w-100 w-70-m db">Ponto Awto 3</label>
-              <input
-                className="formControl center db w-100 w-70-m"
-                type="text"
-                placeholder="Encontrar-me na barra de endereço"
-              />
-              <label className="center  text-color-secondaryfw7 mv2 w-100 w-70-m db">
-                Qual é o seu nome?
-              </label>
-              <input
-                className="formControl center db w-100 w-70-m"
-                type="text"
-                placeholder="Digite seu primeiro e sobrenome"
-              />
-              <label className="center text-color-secondaryfw7 mv2 w-100 w-70-m db">
-                Qual é o seu e-mail?
-              </label>
-              <input className="formControl center db w-100 w-70-m" type="text" placeholder="Digite seu e-mail"
-              />
-              <div className="center db w-100 w-70-m">
-                <input className="btn bg-secondary w-100 w-40-l" type="submit" />
-              </div>
-            </div>
-          </form>
-        </div>
+        <iframe scrolling="no" className="iframe-map" src="https://www.awtosuite.pro/marketing/saopaulo" title="mapa">
+        </iframe>
       </div>
       <div className="modal" id="modal-one">
         <div className="modal-bg modal-exit"></div>
